8b8a8949071dfe1e9084715c71c0fa8b4ef532b7
Rishabh <rishabhbhagat2801@gmail.com>
2023-11-24 19:20:51 -0500

    final commit

---
+++ 3-4/js/script.js
@@ -0,0 +1,13 @@
+function convertToUSD() {
+    var CAD_Input = document.getElementById("CADdollarInput").value;
+    var rate = 0.73; 
+    var USDdollarValue = CAD_Input * rate;
+    document.getElementById("output").innerHTML = "USD: $" + USDdollarValue.toFixed(4);
+}
+
+function convertToCAD() {
+    var USD_Input = document.getElementById("USDdollarInput").value;
+    var rate = 1.36; 
+    var CADdollarValue = USD_Input * rate;
+    document.getElementById("output").innerHTML = "CAD: $" + CADdollarValue.toFixed(4);
+}
